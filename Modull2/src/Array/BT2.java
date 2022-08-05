0b9b905e7950b9e4f44d39f1b42584428f4ff2a8
vokimthanh2002 <vothanh6081@gmail.com>
2022-08-05 23:45:58 +0700

    update

--- Modull2/src/Array/BT2.java
+++ Modull2/src/Array/BT2.java
@@ -1,39 +1,53 @@
 package Array;
-
-import java.util.Scanner;
-
+import java.util.*;
 public class BT2 {
-    public static void main(String[] args) {
-        int size,X,vt;
-        int[] array;
-        Scanner scan = new Scanner(System.in);
-        System.out.print("Ban  muon nhap vao mang bao nhieu so: ");
-        size = scan.nextInt();
-        array = new int[size];
-        for (int i = 0; i < size; i++) {
-            System.out.print("Nhap phan tu thu " + i + " : ");
-            array[i] = scan.nextInt();
-        }
-        for (int i : array) {
-            System.out.print(i + "\t");
-        }
-        System.out.print("Nhap X la so can chen: ");
-        X=scan.nextInt();
-        System.out.print("Nhap vi tri chen: ");
-        vt=scan.nextInt();
-        if(vt<=1 && vt>=array.length-1)
+    private int X,index,number;
+    private int [] array;
+    public static Scanner sc= new Scanner(System.in);
+
+    //Duyet mang
+    public void ExportList()
+    {
+        for(int i=0;i<number+1;i++)
         {
-            System.out.print("Khong chen duoc phan tu vao mang!! ");
+            System.out.print(array[i]+" ");
+        }
+        System.out.print("\n");
+    }
+    public void Add()
+    {
+        for(int i=number-1;i>=index;i--)
+        {
+            array[i+1]= array[i];
+        }
+        array[index]=X;
+    }
+    public void Processing()
+    {
+        System.out.print("Input number array: ");
+        number= sc.nextInt();
+        array= new int[number+1];
+        for(int i=0;i<number;i++)
+        {
+            System.out.print("Input element index " +i+" : ");
+            array[i]=sc.nextInt();
+        }
+        System.out.print("Input number add: ");
+        X=sc.nextInt();
+        System.out.print("Input index: ");
+        index=sc.nextInt();
+        if(index<=1 || index >=number-1)
+        {
+            System.out.print("Not delete!!");
         }else{
-            for(int i=vt; i<array.length;i++)
-            {
-                array[i]=array[i+1];
-            }
-            array[vt]=X;
-            for (int i : array) {
-                System.out.print(i + "\t");
-            }
+            Add();
+            ExportList();
         }
 
-        }
     }
+    public static void main(String[] args)
+    {
+        BT2 add= new BT2();
+        add.Processing();
+    }
+}
